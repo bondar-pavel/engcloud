b3f160c8e0379bea8cff01c193de8b3e5ce02bc8
John Belamaric <jbelamaric@infoblox.com>
2016-02-22 16:11:34 +0000

    Fix flavor memory allocations
    The members were indicating that not all licensed memory was in
    use, so the flavors are now updated to reflect the correct ammounts.

--- scripts/ib-flavors.sh
+++ scripts/ib-flavors.sh
@@ -7,9 +7,9 @@ fi
 
 nova flavor-create --is-public true vnios-100.55 auto 1024 55 1 --swap 0 --ephemeral 0
 nova flavor-create --is-public true vnios-810.55 auto 2048 55 2 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-820.55 auto 3584 55 2 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-CP-V800.160 auto 2052 160 2 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-1410.160 auto 14848 160 4 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-1420.160 auto 14848 160 4 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-2210.160 auto 14848 160 4 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-2220.160 auto 14848 160 4 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-820.55 auto 4096 55 2 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-CP-V800.160 auto 2048 160 2 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-1410.160 auto 15360 160 4 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-1420.160 auto 15360 160 4 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-2210.160 auto 15360 160 4 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-2220.160 auto 15360 160 4 --swap 0 --ephemeral 0
