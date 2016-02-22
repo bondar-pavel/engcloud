22a08f59b2b63e26559b72fb11c9220b14a7db4a
John Belamaric <jbelamaric@infoblox.com>
2016-02-22 04:56:22 +0000

    Changes for connection moved to template
    Connection info has been moved from heat.conf to the templates.
    Also, greatly simplified the setup by doing additional GM config
    and creating a Heat environment with the proper parameters for
    the autoscale template.

---
+++ dns-vnf/README.rst
@@ -0,0 +1,13 @@
+=============================
+DNS-VNF Templates and Scripts
+=============================
+
+Usage
+-----
+
+::
+  $ heat stack-create -f gm.yaml -P"external_network=<your-ext-net>" gm
+  $ heat stack-show gm
+  $ ./config-gm.sh <gm-floating-ip>
+  $ heat stack-create -e gm-<gm-floating-ip>-env.yaml -f autoscale.yaml autoscale
+  $ heat stack-show autoscale
--- dns-vnf/config-gm.sh
+++ dns-vnf/config-gm.sh
@@ -25,17 +25,49 @@ echo | openssl s_client -connect $FIP:443 >/dev/null 2>&1
 wait=$?
 while [ "$wait" -ne "0" ]
 do
-  echo $(date): Could not download certificate...waiting...
+  echo $(date): Could not download certificate yet...waiting...
   sleep 15
   echo | openssl s_client -connect $FIP:443 >/dev/null 2>&1
   wait=$?
 done
 
-echo $(date): Downloading certificate for use in member join
+echo $(date): Downloading certificate for use in member join...
 echo
-echo | openssl s_client -connect $FIP:443 2>/dev/null | openssl x509 | tr -d \\n > gm.cert
-echo $(date): Stored in gm.cert
+echo | openssl s_client -connect $FIP:443 2>/dev/null | openssl x509 | sed -e 's/^/    /' > /tmp/gm-$FIP-cert.pem
+echo $(date): Done
+
+LAN1_IP=$(curl -sk -u admin:infoblox -X GET "https://$FIP/wapi/v2.3/member?host_name=infoblox.localdomain&_return_fields=vip_setting" | grep address | cut -d: -f 2 | tr -d '", ')
+FIP_ID=$(neutron floatingip-list -c id -c floating_ip_address -f value | grep " $FIP\$" | cut -f 1 -d ' ')
+echo "Floating IP ID is $FIP_ID"
+FIP_NET_ID=$(neutron floatingip-show -c floating_network_id -f value $FIP_ID)
+echo "FIP NET ID $FIP_NET_ID"
+FIP_NET=$(neutron net-show -c name -f value $FIP_NET_ID)
+echo "FIP NET $FIP_NET"
+
+cat > gm-$FIP-env.yaml <<EOF
+# Heat environment for launching autoscale against GM $FIP
+parameters:
+  gm_lan1_ip: $LAN1_IP
+  external_network: $FIP_NET
+  gm_cert: |
+EOF
+
+cat >> gm-$FIP-env.yaml < /tmp/gm-$FIP-cert.pem
+
+cat >> gm-$FIP-env.yaml <<EOF
+parameter_defaults:
+  wapi_url: https://$FIP/wapi/v2.3/
+  wapi_username: admin
+  wapi_password: infoblox
+  wapi_sslverify: false
+EOF
+
+echo "Enabling DNS..."
+GM_REF=$(curl -sk -u admin:infoblox https://172.22.138.92/wapi/v2.3/member:dns?host_name=infoblox.localdomain | grep _ref | cut -d: -f2-3 | tr -d '," ')
+echo $(curl -sk -u admin:infoblox -X PUT -H "Content-Type: application/json" -d '{"enable_dns": true}' https://$FIP/wapi/v2.3/$GM_REF)
 
 echo "Adding a default nsgroup..."
+echo $(curl -sk -u admin:infoblox -X POST -H "Content-Type: application/json" -d '{"name": "default", "is_grid_default": true, "grid_primary": [{"name": "infoblox.localdomain"}]}' https://$FIP/wapi/v2.3/nsgroup)
 
-echo $(curl -sk -u admin:infoblox -X POST -H "Content-Type: application/json" -d '{"name": "default", "is_grid_default": true}' https://$FIP/wapi/v2.3/nsgroup)
+echo You may now create the autoscale stack with:
+echo heat stack-create -e gm-$FIP-env.yaml -f autoscale.yaml autoscale
